7436193a94ad6c775f33a6f3c5389e391b0c76e2
singhsanket143 <singhsanket143@gmail.com>
2026-03-13 22:06:25 +0530

    Added problematic create order

--- src/main/java/com/example/FakeCommerce/services/OrderService.java
+++ src/main/java/com/example/FakeCommerce/services/OrderService.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.example.FakeCommerce.adapters.OrderAdapter;
+import com.example.FakeCommerce.dtos.CreateOrderRequestDTO;
 import com.example.FakeCommerce.dtos.GetOrderResponseDto;
 import com.example.FakeCommerce.exceptions.ResourceNotFoundException;
 import com.example.FakeCommerce.repositories.OrderRespository;
 import com.example.FakeCommerce.repositories.OrderproductsRepository;
 import com.example.FakeCommerce.repositories.ProductRepository;
 import com.example.FakeCommerce.schema.Order;
+import com.example.FakeCommerce.schema.OrderProducts;
+import com.example.FakeCommerce.schema.OrderStatus;
+import com.example.FakeCommerce.schema.Product;
 
 import lombok.RequiredArgsConstructor;
 
@@ -44,4 +48,39 @@ public class OrderService {
 
         orderRespository.delete(order);
     }
+
+
+    public void createOrder(CreateOrderRequestDTO createOrderRequestDTO) {
+        Order order = Order.builder()
+                        .status(OrderStatus.PENDING)
+                        .build();
+
+        orderRespository.save(order);
+
+        if(createOrderRequestDTO.getOrderItems() != null) {
+            for(var itemDto : createOrderRequestDTO.getOrderItems()) {
+                Product product = productRepository.findById(itemDto.getProductId())
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found with id: " + itemDto.getProductId()));
+
+
+                OrderProducts orderProduct = OrderProducts.builder()
+                    .order(order)
+                    .product(product)
+                    .quantity(itemDto.getQuantity() != null ? itemDto.getQuantity() : 1)
+                    .build();
+
+                orderproductsRepository.save(orderProduct);
+                
+            }
+        }
+
+
+    }
 }
+
+
+// User -> Cart -> Adds an item -> New Order (Pending)
+
+// User -> adds more items in the cart -> Same order will be updated
+
+// During checkout -> Order Pending -> Success/Failure
