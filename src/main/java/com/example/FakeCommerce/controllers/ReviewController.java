71b0d1bbfca70b34676bd4079e0dd7c9b95daaec
singhsanket143 <singhsanket143@gmail.com>
2026-03-20 21:55:13 +0530

    Added review apis

--- src/main/java/com/example/FakeCommerce/controllers/ReviewController.java
+++ src/main/java/com/example/FakeCommerce/controllers/ReviewController.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.FakeCommerce.dtos.GetReviewResponseDto;
 import com.example.FakeCommerce.schema.Review;
+import com.example.FakeCommerce.services.ReviewService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,33 +21,35 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReviewController {
 
+    private final ReviewService reviewService;
+
     @GetMapping
-    public List<Review> getAllReviews() {
-        throw new UnsupportedOperationException("Not implemented");
+    public List<GetReviewResponseDto> getAllReviews() {
+        return reviewService.getAllReviews();
     }
 
     @PostMapping
-    public Review createReview() {
-        throw new UnsupportedOperationException("Not implemented");
+    public Review createReview(@RequestBody Review review) {
+        return reviewService.createReview(review);
     }
 
     @DeleteMapping("/{id}")
     public void deleteReview(@PathVariable Long id) {
-        throw new UnsupportedOperationException("Not implemented");
+        reviewService.deleteReview(id);
     }
 
     @GetMapping("/{id}")
-    public Review getReviewById(@PathVariable Long id) {
-        throw new UnsupportedOperationException("Not implemented");
+    public GetReviewResponseDto getReviewById(@PathVariable Long id) {
+        return reviewService.getReviewById(id);
     }
 
     @GetMapping("/product/{productId}")
-    public List<Review> getReviewsByProductId(@PathVariable Long productId) {
-        throw new UnsupportedOperationException("Not implemented");
+    public List<GetReviewResponseDto> getReviewsByProductId(@PathVariable Long productId) {
+        return reviewService.getReviewsByProductId(productId);
     }
 
     @GetMapping("/order/{orderId}")
-    public List<Review> getReviewsByOrderId(@PathVariable Long orderId) {
-        throw new UnsupportedOperationException("Not implemented");
+    public List<GetReviewResponseDto> getReviewsByOrderId(@PathVariable Long orderId) {
+        return reviewService.getReviewsByOrderId(orderId);
     }
 }
